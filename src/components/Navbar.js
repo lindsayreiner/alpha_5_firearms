5c102f9d69856d3455272c3b698149803980a7c1
Lindsay Reiner <lindsayreiner@hey.com>
2023-03-22 00:20:48 -0400

    finishing touches on responsive nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
         <>
             <nav class="top-nav">
                 <NavLink className="bizName" to="/">
-                    A5
+                    A5 Firearms Training
                 </NavLink>
                 <input id="menu-toggle" type="checkbox" />
                 <label class='menu-button-container' for="menu-toggle">
@@ -16,19 +16,19 @@ export default function Navbar() {
                 </label>
                 <ul class="menu">
                     <li>
-                        <NavLink className="navBtnLink" to="/">Home</NavLink>
+                        <NavLink className="navBtn" to="/">Home</NavLink>
                     </li>
                     <li>
-                        <NavLink className="navBtnLink" to="https://training.usconcealedcarry.com/instructor/403f6128-6d56-11eb-aad4-02420a020192" target="_blank">Courses</NavLink>
+                        <NavLink className="navBtn" to="https://training.usconcealedcarry.com/instructor/403f6128-6d56-11eb-aad4-02420a020192" target="_blank">Courses</NavLink>
                     </li>
                     <li>
-                        <NavLink className="navBtnLink" to="/about">Meet Your Instructor</NavLink>
+                        <NavLink className="navBtn" to="/about">Meet Your Instructor</NavLink>
                     </li>
                     <li>
-                        <NavLink className="navBtnLink" to="/blog">Blog</NavLink>
+                        <NavLink className="navBtn" to="/blog">Blog</NavLink>
                     </li>
                     <li>
-                        <NavLink className="navBtnLink" to="/contact">Contact</NavLink>
+                        <NavLink className="navBtn" to="/contact">Contact</NavLink>
                     </li>
                 </ul>
             </nav>
